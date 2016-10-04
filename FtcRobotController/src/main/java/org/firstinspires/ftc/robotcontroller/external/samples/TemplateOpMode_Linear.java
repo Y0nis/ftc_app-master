cc6c6a485dd1d91c862fcf6ad6e36f4714919ebb
Y0nis <ikutrolli2018@spyponders.com>
2016-10-04 17:00:37 -0400

    This is just a test.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/TemplateOpMode_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/TemplateOpMode_Linear.java
@@ -33,7 +33,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testTeleop.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Yonis on 10/4/16.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp (name="Test Teleop",group="Test")
+public class testTeleop extends OpMode {
+
+
+     DcMotor leftMotor, rightMotor;
+
+    @Override
+    public void loop(){
+
+
+    }
+
+    @Override
+    public void init(){
+
+
+    }
+
+
+}
