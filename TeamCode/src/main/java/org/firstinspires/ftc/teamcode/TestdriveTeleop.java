81415877c5b15b6c53133f91e655a6c2c1684ac7
Y0nis <ikutrolli2018@spyponders.com>
2016-10-04 14:45:12 -0400

    This is just a test.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestdriveTeleop.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by Yonis on 10/4/16.
+ */
+
+
+@TeleOp(name="Test Drive Teleop", group="Test")  // @Autonomous(...) is the other common choice
+
+public class TestdriveTeleop {
+}
