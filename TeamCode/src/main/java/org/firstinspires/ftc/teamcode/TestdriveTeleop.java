6543afd586ae68e32030e3b0a21220d06ef2862c
Y0nis <ikutrolli2018@spyponders.com>
2016-10-04 15:20:12 -0400

    This is just a test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestdriveTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestdriveTeleop.java
@@ -1,13 +1,38 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorImpl;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 /**
  * Created by Yonis on 10/4/16.
  */
 
 
-@TeleOp(name="Test Drive Teleop", group="Test")  // @Autonomous(...) is the other common choice
+@TeleOp(name="Test Drive Teleop", group="Test")  // @Autonomous(...) is the other common choic'e
 
-public class TestdriveTeleop {
+public class TestdriveTeleop extends OpMode{
+
+    private ElapsedTime time = new ElapsedTime();
+
+    DcMotor leftMotor, rightMotor;
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.dcMotor.get("left motor");
+        rightMotor = hardwareMap.dcMotor.get("right motor");
+    }
+
+    @Override
+    public void loop() {
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftMotor.setPower(1);
+        rightMotor.setPower(1);
+    }
 }
