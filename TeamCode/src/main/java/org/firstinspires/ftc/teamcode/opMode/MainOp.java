05d68ecd153f5b8ab7edec89c2a4af34545284b5
Y0nis <ikutrolli2018@spyponders.com>
2016-10-10 21:48:27 -0400

    Added the servos to this opmode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/MainOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/MainOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.teamcode.controllers.DirectionalControl;
@@ -14,21 +15,23 @@ import org.firstinspires.ftc.teamcode.controllers.DirectionalControl;
 public abstract class MainOp extends OpMode {
 
     public DirectionalControl direction;
-    public DcMotor leftMotor, rightMotor, sweeper;
+    public DcMotor leftMotor, rightMotor, sweeperMotor;
     public TouchSensor touchSensor;
-
+    public Servo armServo;
     @Override
     public void init() {
         // Motors
         leftMotor = hardwareMap.dcMotor.get("left motor");
         rightMotor = hardwareMap.dcMotor.get("right motor");
-        //sweeper = hardwareMap.dcMotor.get("sweeper");
+        //sweeperMotor = hardwareMap.dcMotor.get("sweeper");
 
+        //TODO
         // Touch Sensors
         //touchSensor = hardwareMap.touchSensor.get("touch sensor");
 
-        // Servos
         //TODO
+        // Servos
+        //armServo = hardwareMap.servo.get("arm");
 
         // Controllers
         direction = new DirectionalControl(this);
